b3a706ae90964ef98a36c2469bf6744d9b85352e
LukeJessop <jessop.lucas@gmail.com>
2025-03-30 16:21:30 -0600

    scripting 8 challenge, added animations to a bunch of objects

--- Assets/SpikeMover.cs
+++ Assets/SpikeMover.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class SpikeMover : MonoBehaviour
 {
     // Update is called once per frame
+    public Vector3 rotationPoint;
+    public float rotationSpeed;
     void Update()
     {
-        transform.RotateAround(new Vector3(-0.5f,0.3f,0), Vector3.forward, Time.deltaTime * 10f);
+        transform.RotateAround(rotationPoint, Vector3.forward, Time.deltaTime * rotationSpeed);
+    }
+
+    public void TransformSpike()
+    {
+        if (transform.localScale.x > .5f)
+        {
+            transform.localScale = new Vector3(.75f,.75f,.75f);
+        }
     }
 }
