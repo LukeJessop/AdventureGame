ee531ff70d0ece4ecfd00d804bf6f1ead52efcfc
LukeJessop <jessop.lucas@gmail.com>
2025-03-29 22:50:50 -0600

    Created healthbar functionality

---
+++ Assets/Scripts/HealthScripts/ImageBehaviorScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageBehaviorScript : MonoBehaviour
+{
+    private Image imageObj;
+    public SimpleFloatData dataObj;
+
+    private void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+
+    public void UpdateWithFloatData()
+    {
+        imageObj.fillAmount = dataObj.value;
+    }
+}
