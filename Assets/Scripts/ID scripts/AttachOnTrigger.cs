e034b23cb82ffade080cb48e06fdb8f59ef85a93
LukeJessop <jessop.lucas@gmail.com>
2025-04-12 20:49:13 -0600

    fixed bugs with key and fire trap

--- Assets/Scripts/ID scripts/AttachOnTrigger.cs
+++ Assets/Scripts/ID scripts/AttachOnTrigger.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttachOnTrigger : MonoBehaviour
 {
+    public Id idToAttach;
     public void OnTriggerEnter(Collider other)
     {
         transform.parent = other.transform;
+        other.AddComponent<SimpleIdBehavior>();
+        var idComponent = other.GetComponent<SimpleIdBehavior>();
+        idComponent.id = idToAttach;
     }
 
     public void OnTriggerExit(Collider other)
